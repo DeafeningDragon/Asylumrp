6a4281ff37a81929165b72a7915bc360bcfa6e64
DeafeningDragon <deafeningdragon@gmail.com>
2025-02-18 05:51:21 -0600

    update 3/5
    5:51am

---
+++ [interfacescripts]/boii-pausemenu/server/config_sv.lua
@@ -0,0 +1,21 @@
+----------------------------------
+--<!>-- BOII | DEVELOPMENT --<!>--
+----------------------------------
+
+Config = {}
+-- Discord settings
+Config.Discord = {
+    -- Discord webhooks refer to; https://support.discord.com/hc/en-us/articles/228383668-Intro-to-Webhooks
+    Webhook = 'https://discord.com/api/webhooks/1308888621352485035/WajtlV-jmq7lyNFFKwPveisaNMVcXfZLYcJv-n5uVd6b60wTUB8iL4XQWRF0VSnBWP04',
+    -- Webhook colour can find more here; -- https://www.spycolor.com/
+    Colour = 5098434, -- boii development brand blue 
+    -- Discord bot username;
+    BotData = {
+        Name = '-🛡️- Asylum City REPORTS -🛡️-',
+        Logo = '',
+        Footer = {
+            Text = '© - Asylum city | Development ',
+            Icon = ''
+        },
+    }
+}
